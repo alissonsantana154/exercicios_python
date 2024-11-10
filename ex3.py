e4f4e961ad0d0251468087be4a2bd33841a729f3
Alisson Santana <alissonsantana16608@gmail.com>
2024-11-10 14:40:32 -0300

    EX3 Somando dois números

---
+++ ex3.py
@@ -0,0 +1,7 @@
+# Crie um programa que leia dois números e mostre a soma entre eles.
+
+numero1 = int(input('Digite o primeiro número: '))
+numero2 = int(input('Digite o segundo número: '))
+
+soma = numero1 + numero2
+print(f'A soma dos números {numero1} e {numero2} é: {soma}')
