e3cbfc14441f2a79a27697e6787677d8c4348c1c
Alisson Santana <alissonsantana16608@gmail.com>
2024-11-11 21:57:10 -0300

    Antecessor e Sucessor

---
+++ ex5.py
@@ -0,0 +1,7 @@
+# Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor.
+
+valor = int(input('Digite um número: '))
+sucessor = valor + 1
+antecessor = valor - 1
+
+print(f'Analisando o valor {valor}, seu antecessor é {antecessor} e o sucessor é {sucessor}')
