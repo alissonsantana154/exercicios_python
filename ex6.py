9491d74f39c43410664f306a4f420e56ce9da174
Alisson Santana <alissonsantana16608@gmail.com>
2024-11-11 22:08:41 -0300

    Dobro, Triplo, Raiz Quadrada

---
+++ ex6.py
@@ -0,0 +1,7 @@
+# Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
+
+numero = int(input('Digite um número: '))
+
+print(f'O dobro de {numero} vale {numero * 2}.')
+print(f'O triplo de {numero} vale {numero * 3}.')
+print(f'A raíz quadrada de {numero} é igual a {numero ** 0.5:.2f}.')
