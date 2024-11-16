c1feb7dce27c4482dbde19ba08ff86459fe7277b
Alisson Santana <alissonsantana16608@gmail.com>
2024-11-15 22:40:50 -0300

    conversor de Temperaturas

---
+++ ex14.py
@@ -0,0 +1,6 @@
+# Escreva um programa que converte uma temperatura digitada em °C e coverta para °F.
+
+c = float(input("Informe a temperatura em °C: "))
+f = 1.8 * c + 32
+
+print(f"A temperatura de {c}°C corresponde a {f}°F!")
