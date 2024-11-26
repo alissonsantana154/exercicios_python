c4a3a3da6aea2eeed796cfce2140193c6bbd10ea
Alisson Santana <alissonsantana16608@gmail.com>
2024-11-25 21:07:32 -0300

    Quebrando um número

---
+++ ex16.py
@@ -0,0 +1,7 @@
+# Crie um programa que leia um número Real qualquer pleloi teclado e mostre na tela a sua porção inteira.
+
+numero_real = float(input("Digite um valor: "))
+
+numero = int(numero_real)
+
+print(f"O valor digitado foi {numero_real} e a sua porção inteira é {numero}")
